d4a28e914ee4e4bc538968227e7b9fdf9224b5e5
Yoma <codeyoma@gmail.com>
2023-04-02 19:15:05 +0900

    [done] 06

--- ex06/rps.php
+++ ex06/rps.php
@@ -1,35 +1,33 @@
 <?php
 
-$win = "Congratulations! You won! The computer chose ";
-$lose = "Sorry, you lost. The computer chose ";
-$draw = "It's a draw. The computer chose ";
+$win = "Congratulations! You won! The computer chose";
+$lose = "Sorry, you lost. The computer chose";
+$draw = "It's a draw. The computer chose";
 
-$choices = array("rock.", "paper.", "scissors.");
+$choices = array("rock", "paper", "scissors");
 
 echo "Choose rock, paper, or scissors: ";
 $line = (string)fgets(STDIN);
-// $line = fopen(STDIN, "r");
-// $line = fopen("php://stdin", "r");
+// $line = (string)fopen(STDIN, "r");
+// $line = (string)fopen("php://stdin", "r");
 $line = strtolower($line);
 $line = trim($line);
 
-if ($line == "" || $line == "rock" || $line == "paper" || $line == "scissors") {
-	echo "You must choose rock, paper, or scissors.";
-}else{
+if ($line == "" || !($line == "rock" || $line == "paper" || $line == "scissors")) {
+	printf("\n[Error] You must choose rock, paper, or scissors.\n");
+} else {
 	$computer = $choices[rand(0, 2)];
-	// check for win, lose, or draw
-	if ($line == "rock" && $computer == "scissors.") {
-		echo $win, $computer;
-	} elseif ($line == "paper" && $computer == "rock.") {
-		echo $win, $computer;
-	} elseif ($line == "scissors" && $computer == "paper.") {
-		echo $win, $computer;
-	} elseif ($line == $computer) {
-		echo $draw, $computer;
+	if ($line == $computer) {
+		printf("%s %s.\n", $draw, $computer);
+	} elseif ($line == "rock" && $computer == "scissors") {
+		printf("%s %s.\n", $win, $computer);
+	} elseif ($line == "paper" && $computer == "rock") {
+		printf("%s %s.\n", $win, $computer);
+	} elseif ($line == "scissors" && $computer == "paper") {
+		printf("%s %s.\n", $win, $computer);
 	} else {
-		echo $lose, $computer;
+		printf("%s %s.\n", $lose, $computer);
 	}
 }
-
 
 ?>
